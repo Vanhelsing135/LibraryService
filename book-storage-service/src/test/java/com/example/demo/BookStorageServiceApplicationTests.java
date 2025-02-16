dd71854540c12d0b71482fa1d8480e1b5042d204
Ivan Filon <vanbest27@gmail.com>
2025-02-16 20:08:02 +0300

    Initial commit

---
+++ book-storage-service/src/test/java/com/example/demo/BookStorageServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import com.example.bookstorage.BookStorageServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BookStorageServiceApplication.class)
+class BookStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
